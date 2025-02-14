52bd003d473ad701b5cbb9daea120c4d75c14e71
feiverse <186351189+feiverse@users.noreply.github.com>
2025-02-14 18:35:30 +0100

    Update first_word.c

--- Rank 2/level1/first_word.c
+++ Rank 2/level1/first_word.c
@@ -1,8 +1,43 @@
+a program that takes a string and displays its first word, followed by a newline.
+
+A word is a section of string delimited by spaces/tabs or by the start/end of
+the string.
+
+If the number of parameters is not 1, or if there are no words, simply display
+a newline.
+
+Examples:
+
+$> ./first_word "FOR PONY" | cat -e
+FOR$
+$> ./first_word "this        ...    is sparta, then again, maybe    not" | cat -e
+this$
+$> ./first_word "   " | cat -e
+$
+$> ./first_word "a" "b" | cat -e
+$
+$> ./first_word "  lorem,ipsum  " | cat -e
+lorem,ipsum$
+$>
+
+
+
+
+
 
 #include <unistd.h>
 
 int main(int ac, char **av)
 {
 	int v = 0;
-
-  
+    
+    if (ac == 2)
+    {
+        while (av[1][v] == ' ' || av[1][v] == '\t')
+            V++;
+        while (av[1][v] && av[1][v] != ' ' && av[1][v] != '\t')
+            write(1, &av[1][v], 1);
+    }
+    write(1, "\n", 1);
+    return 0;
+}
