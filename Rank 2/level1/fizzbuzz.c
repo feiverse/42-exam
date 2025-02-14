7dbff42770040305b78eadb4cbf5e69720d47e7e
feiverse <186351189+feiverse@users.noreply.github.com>
2025-02-14 20:06:41 +0100

    Update fizzbuzz.c

--- Rank 2/level1/fizzbuzz.c
+++ Rank 2/level1/fizzbuzz.c
@@ -1,3 +1,37 @@
+Write a program that prints the numbers from 1 to 100, each separated by a
+newline.
+
+If the number is a multiple of 3, it prints 'fizz' instead.
+
+If the number is a multiple of 5, it prints 'buzz' instead.
+
+If the number is both a multiple of 3 and a multiple of 5, it prints 'fizzbuzz' instead.
+
+Example:
+
+$>./fizzbuzz
+1
+2
+fizz
+4
+buzz
+fizz
+7
+8
+fizz
+buzz
+11
+fizz
+13
+14
+fizzbuzz
+[...]
+97
+98
+fizz
+buzz
+$>
+
 
 #include <unistd.h>
 
@@ -10,4 +44,25 @@ void putnbr(char *s)
 void putnbr(int n)
 {
 	if (n > 9)
-		putnbr(n \ 10);
+		putnbr(n / 10);
+    char c = n % 10 + '0');
+    write(1, &c, 1);
+
+int main(void)
+{
+    for (int v = 1; v <= 100; v++)
+    {
+        if (v % 15 == 0)
+            putstr("fizzbuzz");
+        else if (v % 3 == 0)
+            putstr("fizz");
+        else if (v % 5 == 0)
+            putstr("buzz");
+        else
+            putnbr(v);
+        write(1, "\n", 1);
+    }
+    return 0;
+}
+
+
