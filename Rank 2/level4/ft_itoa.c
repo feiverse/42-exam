44abfab0c77fe9c2f5c1d868dac99267108fdd5e
feiverse <186351189+feiverse@users.noreply.github.com>
2025-02-15 02:41:36 +0100

    Create ft_itoa.c

---
+++ Rank 2/level4/ft_itoa.c
@@ -0,0 +1,73 @@
+
+Assignment name  : ft_itoa
+Expected files   : ft_itoa.c
+Allowed functions: malloc
+--------------------------------------------------------------------------------
+
+Write a function that takes an int and converts it to a null-terminated string.
+The function returns the result in a char array that you must allocate.
+
+Your function must be declared as follows:
+
+char	*ft_itoa(int nbr);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#include <stdlib.h>
+
+char    *ft_itoa(int n) 
+{
+    int v = 0;
+    long num = n;
+
+    if (num <= 0) 
+    {
+        v++;
+        num = -num;
+    }
+    long fei = num;
+    while (fei > 0) 
+    {
+        v++;
+        fei /= 10;
+    }
+    char *res = (char *)malloc(v + 1);
+    if (!res)
+        return NULL;
+
+    res[v] = '\0';
+
+    if (n == 0)  // 確保 0 正確處理
+        res[0] = '0';
+
+    if (n < 0)
+        res[0] = '-';
+    while (num > 0) 
+    {
+        res[--v] = (num % 10) + '0';
+        num /= 10;
+    }
+
+    return res;
+}
+
+
+//  
+int main() 
+{
+    char *s = ft_itoa(-12345);
+    printf("%s\n", s);
+    free(s);
+    return 0;
+}
