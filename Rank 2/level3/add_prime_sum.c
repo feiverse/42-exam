ba1beb064dfa689cce092388ef58d2c284e564cc
feiverse <186351189+feiverse@users.noreply.github.com>
2025-02-15 01:52:48 +0100

    Create add_prime_sum.c

---
+++ Rank 2/level3/add_prime_sum.c
@@ -0,0 +1,77 @@
+Assignment name  : add_prime_sum
+Expected files   : add_prime_sum.c
+Allowed functions: write, exit
+--------------------------------------------------------------------------------
+
+Write a program that takes a positive integer as argument and displays the sum
+of all prime numbers inferior or equal to it followed by a newline.
+
+If the number of arguments is not 1, or the argument is not a positive number,
+just display 0 followed by a newline.
+
+Yes, the examples are right.
+
+Examples:
+
+$>./add_prime_sum 5
+10
+$>./add_prime_sum 7 | cat -e
+17$
+$>./add_prime_sum | cat -e
+0$
+$>
+
+  
+
+  
+#include <unistd.h>
+
+int ft_atoi(char *s)
+{
+    int n = 0;
+    while (*s >= '0' && *s <= '9')
+        n = n * 10 + (*s++ - '0');
+    return n;
+}
+  
+int prime(int n)
+{
+    if (n < 2) return 0;
+    for (int v = 2; v * v <= n; v++)
+        if (n % v == 0)
+            return 0;
+    return 1;
+}
+
+void putnbr(int n)
+{
+    if (n > 9)
+        putnbr(n / 10);
+    char  c = (n % 10) + '0';
+    write(1, &c, 1);
+}
+
+int main(int ac, char **av)
+{
+    if (ac != 2)
+    {
+        write(1, "0\n", 2);
+        return 0;
+    }
+    
+    int n = ft_atoi(av[1]);
+    if (n <= 0)
+    {
+        write(1, "0\n", 2);
+        return 0;
+    }
+
+    int sum = 0;
+    for (int v = 2; v <= n; v++)
+        if (prime(v))
+            sum += v;
+    
+    putnbr(sum);
+    write(1, "\n", 1);
+    return 0;
+}
