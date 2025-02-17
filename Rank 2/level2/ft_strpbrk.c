e8b345075451922d0335074857fb1d9856885f78
feiverse <186351189+feiverse@users.noreply.github.com>
2025-02-17 05:16:14 +0100

    Create ft_strpbrk.c

---
+++ Rank 2/level2/ft_strpbrk.c
@@ -0,0 +1,58 @@
+Assignment name	: ft_strpbrk
+Expected files	: ft_strpbrk.c
+Allowed functions: None
+---------------------------------------------------------------
+
+Reproduce exactly the behavior of the function strpbrk
+(man strpbrk).
+
+The function should be prototyped as follows:
+
+char	*ft_strpbrk(const char *s1, const char *s2);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+char *ft_strpbrk(const char *s1, const char *s2)
+{
+    while (*s1)
+    {
+        for (const char *v = s2; *v; v++)
+            if (*s1 == *v)
+                return (char *)s1;
+        s1++;
+    }
+    return (NULL);
+}
+
+
+
+
+
+
+//test
+
+#include <stdio.h>
+
+int main()
+{
+    char str1[] = "Hey fei";
+    char str2[] = "yf";
+    char str3[] = "salut";
+
+    printf("%s\n", ft_strpbrk(str1, str2)); // "y fei"
+    printf("%s\n", ft_strpbrk(str1, str3)); // (null) 
+    
+    return 0;
+}
