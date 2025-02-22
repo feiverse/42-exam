8a15001ef6dcb51c98bc07e7c1f94051bd25e241
feiverse <186351189+feiverse@users.noreply.github.com>
2025-02-22 07:56:14 +0100

    Update rev_wstr.c

--- Rank 2/level4/rev_wstr.c
+++ Rank 2/level4/rev_wstr.c
@@ -3,17 +3,17 @@ Expected files   : rev_wstr.c
 Allowed functions: write, malloc, free
 --------------------------------------------------------------------------------
 
-Write a program that takes a string as a parameter, and prints its words in
+Write a program that takes a string as a parameter, and prints its words in 
 reverse order.
 
-A "word" is a part of the string bounded by spaces and/or tabs, or the
+A "word" is a part of the string bounded by spaces and/or tabs, or the 
 begin/end of the string.
 
-If the number of parameters is different from 1, the program will display
+If the number of parameters is different from 1, the program will display 
 '\n'.
 
-In the parameters that are going to be tested, there won't be any "additional"
-spaces (meaning that there won't be additionnal spaces at the beginning or at
+In the parameters that are going to be tested, there won't be any "additional" 
+spaces (meaning that there won't be additionnal spaces at the beginning or at 
 the end of the string, and words will always be separated by exactly one space).
 
 Examples:
@@ -32,31 +32,39 @@ $>
 
 #include <unistd.h>
 
+void rev_wstr(char *s)
+{
+    int v = 0, arc = 0;
+    int start, end;
+
+    while (s[arc])
+        arc++;
+
+    while (arc > 0)
+    {
+        while (arc > 0 && (s[arc - 1] == ' ' || s[arc - 1] == '\t'))
+            arc--;       
+        end = arc;
+ 
+        while (arc > 0 && s[arc - 1] != ' ' && s[arc - 1] != '\t')
+            arc--;
+        start = arc;
+        
+        v = start;
+        while (v < end)
+            write(1, &s[v++], 1);
+			
+        if (start > 0)
+            write(1, " ", 1);
+    }
+}
+
 int main(int ac, char **av)
 {
     if (ac == 2)
     {
-        char *s = av[1];
-        int v = 0, start, first = 1;
-
-        while (s[v]) v++;
-        v--;
-
-        while (v >= 0)
-        {
-            while (v >= 0 && (s[v] == ' ' || s[v] == '\t')) v--;
-            
-            start = v;
-            while (v >= 0 && s[v] != ' ' && s[v] != '\t') v--;           
-            if (start > v)
-            {
-                if (!first) write(1, " ", 1);
-                write(1, &s[v + 1], start - v);
-                first = 0;
-            }
-        }
+        rev_wstr(av[1]);
     }
     write(1, "\n", 1);
-    return 0;
+    return (0);
 }
-
