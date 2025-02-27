a1a154448c8b26b591af599ce8770549733d8d8b
feiverse <186351189+feiverse@users.noreply.github.com>
2025-02-27 09:34:20 +0100

    Update sort_list.c

--- Rank 2/level4/sort_list.c
+++ Rank 2/level4/sort_list.c
@@ -50,9 +50,9 @@ struct s_list
 
 t_list	*sort_list(t_list *lst, int (*cmp)(int, int))
 {
-    t_list *h = lst;
-    int		t;
-
+    int v;
+	t_list *done = lst;
+	
     if (!lst) 
         return NULL;
 
@@ -60,15 +60,15 @@ t_list	*sort_list(t_list *lst, int (*cmp)(int, int))
     {
         if (!cmp(lst->data, lst->next->data))
         {
-            t = lst->data;
+            v = lst->data;
             lst->data = lst->next->data;
-            lst->next->data = t;
-            lst = h;
+            lst->next->data = v;
+            lst = done;
         }
         else
             lst = lst->next;
     }
-    return h;
+    return done;
 }
 
 
