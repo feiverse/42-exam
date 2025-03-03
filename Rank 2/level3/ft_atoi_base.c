69871d3c1bd51262d711396f325d83849eb49d5a
feiverse <186351189+feiverse@users.noreply.github.com>
2025-03-03 23:00:36 +0100

    Update ft_atoi_base.c

--- Rank 2/level3/ft_atoi_base.c
+++ Rank 2/level3/ft_atoi_base.c
@@ -24,36 +24,34 @@ int	ft_atoi_base(const char *str, int str_base);
 
 
 
-int fei(char c)
+int	fei(char c)
 {
-    if (c >= '0' && c <= '9')
-        return (c - '0');
-    else if (c >= 'a' && c <= 'f')
-        return (c - 'a' + 10);
-    else if (c >= 'A' && c <= 'F')
-        return (c - 'A' + 10);
-    return (-1);
+	if (c >= '0' && c <= '9')
+		return (c - '0');
+	else if (c >= 'a' && c <= 'f')
+		return (c - 'a' + 10);
+	else if (c >= 'A' && c <= 'F')
+		return (c - 'A' + 10);
+	return (-1);
 }
 int	ft_atoi_base(const char *str, int str_base)
 {
-	int res = 0;
-    int sign = 1;
-    int v;
-
-    if (*str == '-')
-    {
-        sign = -1;
-        str++;
-    }
-    while (*str)
-    {
-        v = fei(*str);
-        if (v == -1 || v >= str_base)
-            break;
-        res = res * str_base + v;
-        str++;
-    }
-    return (res * sign);
+	int v, res = 0, sign = 1;
+	
+	if (*str == '-')
+	{
+		sign = -1;
+		str++;
+	}
+	while (*str)
+	{
+		v = fei(*str);
+		if (v == -1 || v >= str_base)
+			break;
+		res = res * str_base + v;
+		str++;
+	}
+	return res * sign;
 }
 
 
