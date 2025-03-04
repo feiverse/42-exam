a663b735fc1c7d66736770ff7cfb41b74b1e585f
feiverse <186351189+feiverse@users.noreply.github.com>
2025-03-04 04:19:50 +0100

    Update rotone.c

--- Rank 2/level1/rotone.c
+++ Rank 2/level1/rotone.c
@@ -32,25 +32,22 @@ $>
 
 #include <unistd.h>
 
-int    main(int ac, char **av)
+int main(int ac, char **av)
 {
-	int    v;
-    char   c;
-    
-    if (ac == 2)
-    {
-        v = 0;
-        while (av[1][v])
-        {
-            c = av[1][v];
-            if (c >= 97 && c <= 121 || c >= 65 && c <= 89)
-                c += 1;
-            else if (c == 122 || c == 90)
-                c -= 25;
-            write(1, &c, 1);
-            v++;
-        }
-    }
-    write(1, "\n", 1);
-    return 0;
+	if (ac == 2)
+	{
+		int v = 0;
+		while (av[1][v])
+		{
+			char c = av[1][v];
+			if (c >= 97 && c <= 121 || c >= 65 && c <= 89)
+				c += 1;
+			else if (c == 122 || c == 90)
+				c -= 25;
+			write(1, &c, 1);
+			v++;
+		}
+	}
+	write(1, "\n", 1);
+	return 0;
 }
