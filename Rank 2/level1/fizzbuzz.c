9dccec1d41da23cda2740cec5d4fbd7a7ef724c5
feiverse <186351189+feiverse@users.noreply.github.com>
2025-03-04 05:18:03 +0100

    Update fizzbuzz.c

--- Rank 2/level1/fizzbuzz.c
+++ Rank 2/level1/fizzbuzz.c
@@ -35,32 +35,33 @@ $>
 
 #include <unistd.h>
 
-void putnbr(char *s)
+void	putnbr(char *s)
 {
 	while (*s)
 		write(1, s++, 1);
 }
 
-void putnbr(int n)
+void	putnbr(int n)
 {
 	if (n > 9)
 		putnbr(n / 10);
-    char c = n % 10 + '0');
-    write(1, &c, 1);
+	char c = n % 10 + '0';
+	write(1, &c, 1);
+}
 
-int main(void)
+int	main(void)
 {
-    for (int v = 1; v <= 100; v++)
-    {
-        if (v % 15 == 0)
-            putstr("fizzbuzz");
-        else if (v % 3 == 0)
-            putstr("fizz");
-        else if (v % 5 == 0)
-            putstr("buzz");
-        else
-            putnbr(v);
-        putstr("\n");
-    }
-    return 0;
+	for (int v = 1; v <= 100; v++)
+	{
+		if (v % 15 == 0)
+			putstr("fizzbuzz");
+		else if (v % 3 == 0)
+			putstr("fizz");
+		else if (v % 5 == 0)
+			putstr("buzz");
+		else
+			putnbr(v);
+		putstr("\n");
+	}
+	return 0;
 }
