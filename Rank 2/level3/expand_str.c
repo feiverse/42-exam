46844264dcb7a371690294d6ce95d28dffaf0f0b
feiverse <186351189+feiverse@users.noreply.github.com>
2025-03-10 03:24:24 +0100

    Update expand_str.c

--- Rank 2/level3/expand_str.c
+++ Rank 2/level3/expand_str.c
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: fwei <fwei@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/03/07 15:23:53 by fwei              #+#    #+#             */
-/*   Updated: 2025/03/07 15:43:43 by fwei             ###   ########.fr       */
+/*   Created: 2025/03/10 02:57:20 by fwei              #+#    #+#             */
+/*   Updated: 2025/03/10 03:22:53 by fwei             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,23 +18,22 @@ void	expand_str(char *s)
 
 	while (s[v] == ' ' || s[v] == '\t')
 		v++;
-	
 	while (s[v])
 	{
-		if (s[v] != ' ' && s[v] != '\t')
+		if (s[v] == ' ' || s[v] == '\t') 
+			space3 = 1;
+		else
 		{
 			if (space3)
 				write(1, "   ", 3);
-			space3 = 1;
-			while (s[v] && s[v] != ' ' && s[v] != '\t')
-				write(1, &s[v++], 1);
+			space3 = 0;
+			write(1, &s[v], 1);
 		}
-		else
-			v++;
+		v++;
 	}
 }
 
-int	main(int ac, char **av)
+int main(int ac, char **av)
 {
 	if (ac == 2 && av[1][0])
 		expand_str(av[1]);
