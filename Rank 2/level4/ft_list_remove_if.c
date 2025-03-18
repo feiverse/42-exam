fb7102fddab7723fd8fdcf3615df9b8f79f76760
feiverse <186351189+feiverse@users.noreply.github.com>
2025-03-18 19:33:34 +0100

    Update ft_list_remove_if.c

--- Rank 2/level4/ft_list_remove_if.c
+++ Rank 2/level4/ft_list_remove_if.c
@@ -21,8 +21,7 @@ typedef struct      s_list
 
 void	ft_list_remove_if(t_list **begin_list, void *data_ref, int (*cmp)())
 {
-	t_list	*glow;
-	t_list	*pure;
+	t_list	*glow, *pure;
 
 	if (!begin_list || !*begin_list || !cmp)
 		return ;
