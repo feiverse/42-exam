01c5ae765e4b7ca22d2bfce83b02561e5a21fbd5
feiverse <186351189+feiverse@users.noreply.github.com>
2025-06-05 06:34:17 +0200

    add

--- Rank 3/get_next_line.c
+++ Rank 3/get_next_line.c
@@ -1,96 +1,61 @@
-#include <unistd.h>
-#include <stdlib.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include <fcntl.h>
 
-#ifndef BUFFER_SIZE
-# define BUFFER_SIZE 37
-#endif
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 37
+# endif
 
-int findv(const char *soul)
+char	*btc(char *fei)
 {
-	if (!soul) return -1;
-	for (int v = 0; soul[v]; v++)
-		if (soul[v] == '\n')
-			return v;
-	return -1;
-}
+	char	*v;
+	int		g;
 
-char *linkv(char *gold, char *glow, int v)
-{
-	int ag = 0, ti = 0, pt = 0;
-	while (gold && gold[pt]) pt++;
-	char *fei = malloc(pt + v + 1);
-	if (!fei) return NULL;
-	while (ag < pt)
+	g = 0;
+	while (fei[g])
+		g++;
+	v = (char *)malloc(sizeof(char) * (g + 1));
+	g = 0;
+	while (fei[g])
 	{
-		fei[ag] = gold[ag];
-		ag++;
+	   v[g] = fei[g];
+	   g++;
 	}
-	while (ti < v) 
-	{
-		fei[ag++] = glow[ti++]; 
-	}
-	fei[ag] = '\0';
-	free(gold);
-	return fei;
-}
-
-char *takev(char **soul, int fei)
-{
-	char *lsv;
-	int v = 0;
-	lsv = malloc(fei + 2);
-	if (!lsv) return NULL;
-	while (v <= fei)
-	{
-		lsv[v] = (*soul)[v];
-		v++;
-	}
-	lsv[v] = '\0';
-
-	int dmv = 0;
-	while ((*soul)[v + dmv]) dmv++;
-	if (dmv == 0)
-	{
-		free(*soul); *soul = NULL;
-		return lsv;
-	}
-
-	char *aura = malloc(dmv + 1);
-	if (!aura)
-		return free(lsv), NULL;
-	for (int w = 0; w < dmv; w++)
-		aura[w] = (*soul)[v + w];
-	aura[dmv] = '\0';
-	free(*soul);
-	*soul = aura;
-	return lsv;
+	v[g] = '\0';
+	return (v);
 }
 
 char	*get_next_line(int fd)
 {
-	static char	*aura;
-	char	nova[BUFFER_SIZE];
-	int		btc;
-	int		lsv;
+	static char	buffer[BUFFER_SIZE];
+	char		lsv[77777];
+	static int	v_v;
+	static int 	o_o;
+	int			v;
 
+	v = 0;
 	if (fd < 0 || BUFFER_SIZE <= 0)
-		return NULL;
-	while ((lsv = findv(aura)) == -1)
+		return (NULL);
+	while (1)
 	{
-		btc = read(fd, nova, BUFFER_SIZE);
-		if (btc < 0) return free(aura), aura = NULL, NULL;
-		if (btc == 0) break;
-		aura = linkv(aura, nova, btc);
-		if (!aura) return NULL;
+		if (o_o >= v_v)
+		{
+			v_v = read(fd, buffer, BUFFER_SIZE);
+			o_o = 0;
+			if (v_v <= 0)
+				break ;
+		}
+		lsv[v] = '\0';
+		if (lsv[v] == '\n')
+		{
+			break ;
+		}
+		lsv[v] = buffer[o_o++];
+		v++;
 	}
-	if (!aura || *aura == '\0')
-		return free(aura), aura = NULL, NULL;
-	if (lsv >= 0)
-		return takev(&aura, lsv);
-	else
-	{
-		char *done = aura;
-		aura = NULL;
-		return done;
-	}
+	lsv[v] = '\0';
+	if (v == 0)
+		return (NULL);
+	return (btc(lsv));
 }
